27710820c33a1339e1326be8fe16a3643095942c
Michael Russo <mrusso@smartprocure.us>
2016-09-16 07:59:57 -0500

    disabled autogrow
    ...unless explicitly enabled in settings

--- dist/js/standalone/selectize.js
+++ dist/js/standalone/selectize.js
@@ -1308,7 +1308,7 @@
 			$dropdown.on('mouseenter', '[data-selectable]', function() { return self.onOptionHover.apply(self, arguments); });
 			$dropdown.on('mousedown click', '[data-selectable]', function() { return self.onOptionSelect.apply(self, arguments); });
 			watchChildEvent($control, 'mousedown', '*:not(input)', function() { return self.onItemSelect.apply(self, arguments); });
-			autoGrow($control_input);
+			if (settings.autogrow) autoGrow($control_input);
 	
 			$control.on({
 				mousedown : function() { return self.onMouseDown.apply(self, arguments); },
